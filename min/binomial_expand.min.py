c192afda0d4afc4cb46c4cc20b53362a32ca39fc
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-09-28 11:50:11 +0100

    reminify binomial

--- min/binomial_expand.min.py
+++ min/binomial_expand.min.py
@@ -1,24 +1,20 @@
-Q=range
-L='n'
-K=int
+K='n'
+J=int
 F=input
 C=print
 from math import comb as G
 if __name__=='__main__':
 	C('(a + b)^n\n');H=F('Would you like to sub into a and b? (y/N) ').lower()=='y'
-	if H:D=K(F('a: '));E=K(F('b: '))
+	if H:D=J(F('a: '));E=J(F('b: '))
 	else:D='a';E='b'
-	B=K(F('n: '));C();C(f"Using ({D} + {E})^{B}\n");M=not F('Would you like to condense down the multiplication? (Y/n) ').lower()==L;N=not F('Would you like to print each term on a new line? (Y/n) ').lower()==L;O='\n+ 'if N else' + ';C()
-	for A in Q(B+1):
-		I=O if A!=B else'\n'
-		if M:
-			if H:C(f"[{G(B,A)}({D})^{B-A}][({E})^{A}]",end=I)
-			else:C(f"({G(B,A)}{D}^{B-A})({E}^{A})",end=I)
-		else:C(f"{D}^{B-A} * {E}^{A} * {G(B,A)}",end=I)
+	A=J(F('n: '));C();C(f"Using ({D} + {E})^{A}\n");L=not F('Would you like to condense down the multiplication? (Y/n) ').lower()==K;M=not F('Would you like to print each term on a new line? (Y/n) ').lower()==K;N='\n+ 'if M else' + ';C()
+	for B in range(A+1):
+		I=N if B!=A else'\n'
+		if L:
+			if H:C(f"[{G(A,B)}({D})^{A-B}][({E})^{B}]",end=I)
+			else:C(f"({G(A,B)}{D}^{A-B})({E}^{B})",end=I)
+		else:C(f"{D}^{A-B} * {E}^{B} * {G(A,B)}",end=I)
 	C()
 	if H:
-		P=not F('Would you like to compute the result? (Y/n) ').lower()==L;C()
-		if P:
-			J=0
-			for A in Q(B+1):J+=G(B,A)*D**(B-A)*E**A
-			C(f"Result: {J}")
+		O=not F('Would you like to compute the result? (Y/n) ').lower()==K;C()
+		if O:C(f"Result: {(D+E)**A}")
