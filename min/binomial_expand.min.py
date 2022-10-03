b4df5e611ce9a31e0e132859433ca8a993352bc1
minifier-action <runner@fv-az190-137.22yfuiovmbsufjzmhpaxkig25b.bx.internal.cloudapp.net>
2022-10-03 16:53:58 +0000

    auto-minify

--- min/binomial_expand.min.py
+++ min/binomial_expand.min.py
@@ -1,15 +1,27 @@
 from math import comb
+def smart_comb(n,i):
+	if i==0 or i==n:return 1
+	return comb(n,i)
 if __name__=='__main__':
 	print('(a + b)^n\n');sub=input('Would you like to sub into a and b? (y/N) ').lower()=='y'
-	if sub:a=int(input('a: '));b=int(input('b: '))
+	if sub:
+		while True:
+			try:a=int(input('a: '));break
+			except ValueError:print('a must be a valid integer')
+		while True:
+			try:b=int(input('b: '));break
+			except ValueError:print('b must be a valid integer')
 	else:a='a';b='b'
-	n=int(input('n: '));print();print(f"Using ({a} + {b})^{n}\n");condense=not input('Would you like to condense down the multiplication? (Y/n) ').lower()=='n';newline=not input('Would you like to print each term on a new line? (Y/n) ').lower()=='n';preend='\n+ 'if newline else' + ';print()
+	while True:
+		try:n=int(input('n: '));break
+		except ValueError:print('n must be a valid integer')
+	print();print(f"Using ({a} + {b})^{n}\n");condense=not input('Would you like to condense down the multiplication? (Y/n) ').lower()=='n';newline=not input('Would you like to print each term on a new line? (Y/n) ').lower()=='n';preend='\n+ 'if newline else' + ';print()
 	for i in range(n+1):
 		end=preend if i!=n else'\n'
 		if condense:
-			if sub:print(f"[{comb(n,i)}({a})^{n-i}][({b})^{i}]",end=end)
-			else:print(f"({comb(n,i)}{a}^{n-i})({b}^{i})",end=end)
-		else:print(f"{a}^{n-i} * {b}^{i} * {comb(n,i)}",end=end)
+			if sub:print(f"[{smart_comb(n,i)}({a})^{n-i}][({b})^{i}]",end=end)
+			else:print(f"({smart_comb(n,i)}{a}^{n-i})({b}^{i})",end=end)
+		else:print(f"{a}^{n-i} * {b}^{i} * {smart_comb(n,i)}",end=end)
 	print()
 	if sub:
 		compute=not input('Would you like to compute the result? (Y/n) ').lower()=='n';print()
--- min/pascal.min.py
+++ min/pascal.min.py
@@ -1,5 +1,8 @@
 from math import comb
-def line(n):return[str(comb(n,A))for A in range(n+1)]
+def smart_comb(n,i):
+	if i==0 or i==n:return 1
+	return comb(n,i)
+def line(n):return[str(smart_comb(n,A))for A in range(n+1)]
 def pascal(n):
 	A=' '.join(line(n));B=len(A)
 	for C in range(n):D=' '.join(line(C));print(D.center(B))
