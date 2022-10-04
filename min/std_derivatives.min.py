1358851aa11dc96f2809103f678d75b6ecda5c55
minifier-action <runner@fv-az208-133.e0tah0prcmautdldtlfxyeofwf.cx.internal.cloudapp.net>
2022-10-04 19:21:59 +0000

    auto-minify

---
+++ min/std_derivatives.min.py
@@ -0,0 +1,19 @@
+_F='cot'
+_E='csc'
+_D='sec'
+_C='tan'
+_B='sin'
+_A='cos'
+from math import sin,cos,tan,log,e
+ln=lambda x:log(x,e)
+sec=lambda x:1/cos(x)
+csc=lambda x:1/sin(x)
+cot=lambda x:1/tan(x)
+funcs_2_names={sin:_B,cos:_A,tan:_C,sec:_D,csc:_E,cot:_F,ln:'ln',e:'e'}
+derivative_func={sin:cos,cos:lambda x:-sin(x),tan:lambda x:1/cos(x)**2,sec:lambda x:sec(x)*tan(x),csc:lambda x:-csc(x)*cot(x),cot:lambda x:-1/sin(x)**2,ln:lambda x:1/x,e:lambda x:e**x}
+derivative_str={_B:_A,_A:'-sin',_C:'sec^2',_D:'sec tan',_E:'-csc cot',_F:'-csc^2','ln':'1/x','e':'e^x'}
+def template_derivative(func,x):A={_B:f"cos{x}",_A:f"-sin{x}",_C:f"sec^2{x}",_D:f"sec{x}tan{x}",_E:f"-csc{x}cot{x}",_F:f"-csc^2{x}",'ln':f"1/{x}",'e':f"e^{x}"};return A[func]
+def soft_derivative(func,x,h=0.0001):return(func(x+h)-func(x))/h
+def std_derivative(func,x):return derivative_func[func](x)
+if __name__=='__main__':
+	for func in derivative_func:print(f"{funcs_2_names[func]}'(x) = {template_derivative(funcs_2_names[func],'(x)')}")
