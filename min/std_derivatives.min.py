6d3e1fa0950738fea514e068aa5f8b2d065a99f2
minifier-action <runner@fv-az264-177.ca02foy5wcnuvlrvrz2wi3p2og.dx.internal.cloudapp.net>
2022-10-05 15:09:51 +0000

    auto-minify

--- min/std_derivatives.min.py
+++ min/std_derivatives.min.py
@@ -16,4 +16,13 @@ def template_derivative(func,x):A={_B:f"cos{x}",_A:f"-sin{x}",_C:f"sec^2{x}",_D:
 def soft_derivative(func,x,h=0.0001):return(func(x+h)-func(x))/h
 def std_derivative(func,x):return derivative_func[func](x)
 if __name__=='__main__':
-	for func in derivative_func:print(f"{funcs_2_names[func]}'(x) = {template_derivative(funcs_2_names[func],'(x)')}")
+	for (i,func) in enumerate(derivative_func):print(f"{i+1}) {funcs_2_names[func]}'(x) = {template_derivative(funcs_2_names[func],'(x)')}")
+	print()
+	while True:
+		option=input('Select a function by number (leave blank to exit): ')
+		if option=='':break
+		try:
+			option=int(option)
+			if option not in range(1,len(derivative_func)+1):raise ValueError
+		except ValueError:print('Invalid option');continue
+		func=list(derivative_func)[option-1];x=float(input('Enter a value for x: '));print(f"{funcs_2_names[func]}'({x}) = {std_derivative(func,x)}")
